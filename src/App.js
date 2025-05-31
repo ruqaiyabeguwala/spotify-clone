b2c59c9e71fa4adbb696122805498d73351c147f
Ruqaiya Beguwala <ruqaiya.beguwala@gmail.com>
2025-05-31 12:55:25 +0530

    update

--- src/App.js
+++ src/App.js
@@ -32,9 +32,11 @@ class App extends Component {
     }
 
     if (!hashParams.access_token) {
+      console.log("No access token found, redirecting to Spotify login");
       window.location.href =
-        'https://accounts.spotify.com/authorize?client_id=230be2f46909426b8b80cac36446b52a&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://ruqaiyabeguwala.github.io/spotify-clone/callback';
+        'https://accounts.spotify.com/authorize?client_id=230be2f46909426b8b80cac36446b52a&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
     } else {
+      console.log("Access token found, setting token in Redux store");
       this.props.setToken(hashParams.access_token);
     }
   }
