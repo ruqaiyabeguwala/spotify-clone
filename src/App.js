03a52f10634ca448abbbe80d4806a1e211b807d8
Ruqaiya Beguwala <ruqaiya.beguwala@gmail.com>
2025-05-31 14:47:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
     if (!hashParams.access_token) {
       console.log("No access token found, redirecting to Spotify login");
       window.location.href =
-        'https://accounts.spotify.com/authorize?client_id=faa74af0b21b4f638aaf54314dd6f64d&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://ruqaiyabeguwala.github.io/spotify-clone/callback';
+        'https://accounts.spotify.com/authorize?client_id=faa74af0b21b4f638aaf54314dd6f64d&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=code&redirect_uri=https://ruqaiyabeguwala.github.io/spotify-clone/callback';
     } else {
       console.log("Access token found, setting token in Redux store");
       this.props.setToken(hashParams.access_token);
